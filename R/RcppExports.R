9ff2ffddbbf7342b8e93d98d6803d841ad7e0b72
piterii <59144548+piterii@users.noreply.github.com>
2020-01-17 16:36:06 +0100

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 OptionPricer <- function(S, r, v, t, b, k) {
-    .Call(`_OptionP_OptionPricer`, S, r, v, t, b, k)
+    .Call(`OptionP_OptionPricer`, S, r, v, t, b, k)
 }
 
